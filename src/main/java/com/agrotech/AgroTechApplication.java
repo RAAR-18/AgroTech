ca802ea6bd514e639e671e6c0c218cf3994686b6
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-04-27 11:23:06 -0500

    Initial commit

---
+++ src/main/java/com/agrotech/AgroTechApplication.java
@@ -0,0 +1,13 @@
+package com.agrotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroTechApplication.class, args);
+	}
+
+}
