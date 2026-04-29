5ffc28d6f36a198485c408cd7d123c6e3ec75124
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-04-29 11:11:07 -0500

    (fix) Elimine metodo buscarpornumlotes

--- src/main/java/com/agrotech/repository/CultivoRepository.java
+++ src/main/java/com/agrotech/repository/CultivoRepository.java
@@ -14,12 +14,10 @@ public interface CultivoRepository extends JpaRepository<Cultivo, Integer> {
     // Buscar cultivo por nombre sin distinción de mayúscula
     List<Cultivo> findByNombreContainingIgnoreCase(String nombre);
 
-    // Buscar cultivo por numero de lote
-    List<Cultivo> findByNumeroLote(Integer numeroLote);
-
     // Buscar cultivos por tipo de cultivo
-    @Query("SELECT c FROM Cultivo c JOIN c.tiposCultivos t WHERE t.idTiposCultivos = :idTipo")
-    List<Cultivo> findByTipoCultivoId(@Param("idTipo") Integer idTipo);
+    List<Cultivo> findByTipoCultivo_IdTiposCultivos(Integer idTipoCultivo);
+
+    List<Cultivo> findByTipoCultivo_NombreContainingIgnoreCase(String nombreTipoCultivo);
 
     @Query("SELECT c FROM Cultivo c JOIN c.siembras s WHERE s.finca.idFinca = :idFinca")
     List<Cultivo> findByFincaId(@Param("idFinca") Integer idFinca);
