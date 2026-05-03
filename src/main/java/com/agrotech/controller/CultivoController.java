abeee0a7ddc21aaf152fdf88f2d2afddabfb614f
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-05-02 19:53:53 -0500

    (fix) cambiando nombres a ingles

--- src/main/java/com/agrotech/controller/CultivoController.java
+++ src/main/java/com/agrotech/controller/CultivoController.java
@@ -1,8 +1,8 @@
 package com.agrotech.controller;
 
-import com.agrotech.dto.request.CultivoRequest;
-import com.agrotech.dto.response.CultivoResponse;
-import com.agrotech.servicio.CultivoServicio;
+import com.agrotech.dto.request.CultivoRequestDTO;
+import com.agrotech.dto.response.CultivoResponseDTO;
+import com.agrotech.service.CultivoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,24 +14,24 @@ import java.util.List;
 @RequestMapping("/api/cultivos")
 public class CultivoController {
 
-    private final CultivoServicio cultivoServicio;
+    private final CultivoService cultivoService;
 
-    public CultivoController(CultivoServicio cultivoServicio) {
-        this.cultivoServicio = cultivoServicio;
+    public CultivoController(CultivoService cultivoService) {
+        this.cultivoService = cultivoService;
     }
 
     // Crear Cultivo
     // /api/cultivos
     @PostMapping
-    public ResponseEntity<CultivoResponse> crearCultivo(@Valid @RequestBody CultivoRequest cultivoRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(cultivoServicio.crear(cultivoRequest));
+    public ResponseEntity<CultivoResponseDTO> crearCultivo(@Valid @RequestBody CultivoRequestDTO cultivoRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cultivoService.crear(cultivoRequestDTO));
     }
 
     // Listar Cultivos
     // /api/cultivos
     @GetMapping
-    public ResponseEntity<List<CultivoResponse>> listarCultivos() {
-        return ResponseEntity.ok(cultivoServicio.listarTodos());
+    public ResponseEntity<List<CultivoResponseDTO>> listarCultivos() {
+        return ResponseEntity.ok(cultivoService.listarTodos());
     }
 
     /*
@@ -55,15 +55,15 @@ public class CultivoController {
     // Editar cultivo
     // /api/cultivos/{id}
     @PutMapping("/{id}")
-    public ResponseEntity<CultivoResponse> actualizarCultivo(@PathVariable Integer id, @Valid @RequestBody CultivoRequest cultivoRequest) {
-        return ResponseEntity.ok(cultivoServicio.actualizar(id, cultivoRequest));
+    public ResponseEntity<CultivoResponseDTO> actualizarCultivo(@PathVariable Integer id, @Valid @RequestBody CultivoRequestDTO cultivoRequestDTO) {
+        return ResponseEntity.ok(cultivoService.actualizar(id, cultivoRequestDTO));
     }
 
     // Eliminar cultivo
     // /api/cultivos/{id}
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarCultivo(@PathVariable Integer id) {
-        cultivoServicio.eliminar(id);
+        cultivoService.eliminar(id);
         return ResponseEntity.noContent().build();
     }
 
