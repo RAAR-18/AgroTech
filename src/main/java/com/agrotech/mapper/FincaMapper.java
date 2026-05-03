5f885400f6e2ce3a1f2d099d148af7b9b4a20b05
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-05-02 19:57:45 -0500

    (feat) add validacion para actualizar

--- src/main/java/com/agrotech/mapper/FincaMapper.java
+++ src/main/java/com/agrotech/mapper/FincaMapper.java
@@ -1,12 +1,10 @@
 package com.agrotech.mapper;
 
 import com.agrotech.Entity.Finca;
-import com.agrotech.dto.request.FincaRequest;
-import com.agrotech.dto.request.FincaUpdateRequest;
-import com.agrotech.dto.response.FincaResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import com.agrotech.dto.request.FincaRequestDTO;
+import com.agrotech.dto.request.FincaUpdateRequestDTO;
+import com.agrotech.dto.response.FincaResponseDTO;
+import org.mapstruct.*;
 
 import java.math.BigDecimal;
 
@@ -18,21 +16,27 @@ public interface FincaMapper {
     @Mapping(target = "productor", ignore = true) // Ignorar el productor, se asignará en el servicio
     @Mapping(target = "ubicacion", ignore = true)
     @Mapping(target = "siembras", ignore = true) // Ignorar las siembras, se gestionarán por separado
-    @Mapping(target = "hectareas", expression = "java(toBigDecimal(fincaRequest.getHectareas()))") // Convertir String a BigDecimal
-    Finca toEntity(FincaRequest fincaRequest);
+    @Mapping(target = "hectareas", expression = "java(toBigDecimal(fincaRequestDTO.getHectareas()))") // Convertir String a BigDecimal
+    Finca toEntity(FincaRequestDTO fincaRequestDTO);
 
     // Finca -> FincaResponse
     @Mapping(source = "ubicacion.nombre", target = "nombreUbicacion") // Mapear el nombre de la ubicación
     @Mapping(target = "hectareas", expression = "java(toDouble(finca.getHectareas()))") // Convertir BigDecimal a String
-    FincaResponse toResponse(Finca finca);
+    FincaResponseDTO toResponse(Finca finca);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "idFinca", ignore = true) // Ignorar el ID al actualizar
     @Mapping(target = "productor", ignore = true) // Ignorar el productor,
     @Mapping(target = "ubicacion", ignore = true)
     @Mapping(target = "siembras", ignore = true)
-    @Mapping(target = "hectareas", expression = "java(toBigDecimal(fincaUpdateRequest.getHectareas()))") // Convertir String a BigDecimal
-    void updateEntityFromRequest(FincaUpdateRequest fincaUpdateRequest, @MappingTarget Finca finca);
+    @Mapping(target = "hectareas", ignore = true)
+    void updateEntityFromRequest(FincaUpdateRequestDTO fincaUpdateRequestDTO, @MappingTarget Finca finca);
 
+    default void updateHectareas(FincaUpdateRequestDTO dto, Finca finca) {
+        if (dto.getHectareas() != null) {
+            finca.setHectareas(BigDecimal.valueOf(dto.getHectareas()));
+        }
+    }
 
     // Conversión Double -> BigDecimal
     default BigDecimal toBigDecimal(Double value) {
