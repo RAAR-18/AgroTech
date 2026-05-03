482ba0af29f35bf8bfdb42f40ba549e22bef832b
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-05-02 20:04:59 -0500

    (fix) cambio de nombre

---
+++ src/main/java/com/agrotech/service/SiembraService.java
@@ -0,0 +1,19 @@
+package com.agrotech.service;
+
+import com.agrotech.dto.request.SiembraRequestDTO;
+import com.agrotech.dto.response.SiembraResponseDTO;
+
+import java.util.List;
+
+public interface SiembraService {
+
+    public SiembraResponseDTO crear(SiembraRequestDTO siembraRequestDTO);
+
+    List<SiembraResponseDTO> listar();
+
+    List<SiembraResponseDTO> bucarPorFinca(Integer idFinca);
+
+    List<SiembraResponseDTO> bucarPorCultivo(Integer idCultivo);
+
+    void eliminar(Integer idSiembra);
+}
